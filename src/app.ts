2d015f549e4e4d3da5fa311c14ea8ca5cd0b4318
prakhar011 <prakharawasthi.pa57@gmail.com>
2022-06-02 13:34:54 +0530

    added rate limit for ddos protection

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,21 @@ import config from "config";
 import connect from './utils/connect';
 import logger from './utils/logger';
 import routes from './routes';
+import rateLimit from 'express-rate-limit'
+
+const limiter = rateLimit({
+	windowMs: 5 * 60 * 1000, 
+	max: 100,
+	standardHeaders: true, 
+	legacyHeaders: false,
+})
+
 
 const port = config.get<number>("port");
 
 const app = express();
 app.use(express.json());
+app.use(limiter);
 app.listen(port, async () => {
     logger.info(`Server started on port ${port}`);
     connect();
